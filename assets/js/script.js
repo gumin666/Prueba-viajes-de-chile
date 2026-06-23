dc2ecffcf4d71c8ee8c6c75060da1368cbf5b77b
laura figueroa <laurigumi@gmail.com>
2026-06-23 14:34:01 -0400

    correcciones en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,12 @@
-
+//notificacion "mensaje enviado"
 document.getElementById("formulario").addEventListener("submit", function(e){
-
-    e.preventDefault();
-
+e.preventDefault();
     alert("Mensaje enviado correctamente");
-
 });
-
-
-
 
 
 var elementos = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 
 elementos.forEach(function(item){
-
     new bootstrap.Tooltip(item);
-
 });
