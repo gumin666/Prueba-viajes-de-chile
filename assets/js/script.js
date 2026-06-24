fcfd3f72ca88c2482033a7268922cf07ab09e868
laura figueroa <laurigumi@gmail.com>
2026-06-24 00:28:39 -0400

    actualizacion jquery, cambios en css

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,21 @@ var elementos = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 elementos.forEach(function(item){
     new bootstrap.Tooltip(item);
 });
+
+
+//croll
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 70
+        },
+        800
+      );
+    }
+  });
+});
